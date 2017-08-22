8f8a83479dd75765b43e7860f01768a6a8e294e2
harshsrt <harsh.harsh17@gmail.com>
2017-08-22 08:00:37 +0530

    First commit

---
+++ MavenProjectSelenium/src/test/java/FirstMaven/FirstClass.java
@@ -0,0 +1,34 @@
+package FirstMaven;
+
+import org.testng.annotations.Test;
+
+public class FirstClass {
+
+	@Test
+ public void helloworld1()
+ {
+	System.out.println("Hello world1"); 
+ }
+	@Test
+	 public void helloworld2()
+	 {
+		System.out.println("Hello world2"); 
+	 }
+	@Test
+	 public void helloworld3()
+	 {
+		System.out.println("Hello world3"); 
+	 }
+	@Test
+	 public void helloworld4()
+	 {
+		System.out.println("Hello world"); 
+	 }
+	@Test
+	 public void helloworld5()
+	 {
+		System.out.println("Hello world"); 
+	 }
+	
+	
+}
