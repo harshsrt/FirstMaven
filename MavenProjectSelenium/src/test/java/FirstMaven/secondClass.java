ff97ca9a0f84986936b9e41752a19ccfc810888c
harshsrt <harsh.harsh17@gmail.com>
2017-08-24 23:53:14 +0530

    commit11

--- MavenProjectSelenium/src/test/java/FirstMaven/secondClass.java
+++ MavenProjectSelenium/src/test/java/FirstMaven/secondClass.java
@@ -5,30 +5,30 @@ import org.testng.annotations.Test;
 public class secondClass {
 
 	@Test
- public void helloworld1()
+ public void helloworld11()
  {
 	System.out.println("Hello world1"); 
  }
 	//second commit
 	@Test
-	 public void helloworld2()
+	 public void helloworld12()
 	 {
 		System.out.println("Hello world2"); 
 	 }
 	//third commit
 	@Test
-	 public void helloworld3()
+	 public void helloworld13()
 	 {
 		System.out.println("Hello world3"); 
 	 }
 	//fourth commit
 	@Test
-	 public void helloworld4()
+	 public void helloworld14()
 	 {
 		System.out.println("Hello world"); 
 	 }
 	@Test
-	 public void helloworld5()
+	 public void helloworld15()
 	 {
 		System.out.println("Jai Hind"); 
 	 }
