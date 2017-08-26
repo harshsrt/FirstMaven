98aa3e152f35bb3344be65302510746d60555128
harshsrt <harsh.harsh17@gmail.com>
2017-08-26 08:23:47 +0530

    commit14

--- MavenProjectSelenium/src/test/java/FirstMaven/FirstClass.java
+++ MavenProjectSelenium/src/test/java/FirstMaven/FirstClass.java
@@ -32,5 +32,33 @@ public class FirstClass {
 	 {
 		System.out.println("Jai Hind"); 
 	 }
-	
+	@Test
+	 public void helloworld10()
+	 {
+		System.out.println("Hello world1"); 
+	 }
+		//second commit
+		@Test
+		 public void helloworld20()
+		 {
+			System.out.println("Hello world2"); 
+		 }
+		//third commit
+		@Test
+		 public void helloworld30()
+		 {
+			System.out.println("Hello world3"); 
+		 }
+		//fourth commit
+		@Test
+		 public void helloworld40()
+		 {
+			System.out.println("Hello world"); 
+		 }
+		@Test
+		 public void helloworld50()
+		 {
+			System.out.println("Jai Hind"); 
+		 }
+		
 }
